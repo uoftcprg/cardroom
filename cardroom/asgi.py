fd102b9caa1b2812bb81c78a714bb98276dca2e0
Juho Kim <juho-kim@outlook.com>
2024-02-11 05:48:41 -0500

    Add AllowedHostsOriginValidator

--- cardroom/asgi.py
+++ cardroom/asgi.py
@@ -2,6 +2,7 @@ from importlib import import_module
 
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
 from cardroom.middleware import TokenAuthMiddlewareStack
@@ -10,9 +11,13 @@ from cardroom.utilities import get_root_routingconf
 application = ProtocolTypeRouter(
     {
         'http': get_asgi_application(),
-        'websocket': AuthMiddlewareStack(
-            TokenAuthMiddlewareStack(
-                URLRouter(import_module(get_root_routingconf()).urlpatterns),
+        'websocket': AllowedHostsOriginValidator(
+            AuthMiddlewareStack(
+                TokenAuthMiddlewareStack(
+                    URLRouter(
+                        import_module(get_root_routingconf()).urlpatterns,
+                    ),
+                ),
             ),
         )
     },
